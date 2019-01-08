2dcd129c939f10a2a2635c89d0afdab79e4148ff
Haur Kang <haur.kang@gmail.com>
2019-01-08 22:23:53 +0000

    creates state for app and todos component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component {
+    state = {
+      todos: [
+        {
+          id: 1,
+          title: 'take the rubbish out',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Have a great day',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Rogers likes Tarquin',
+          completed: false
+        },
+      ]
+    }
   render() {
     return (
       <div className="App">
-          App
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
